7a096103a46c39eb1b6f1fb4940102f3aca60965
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2019-01-20 23:24:19 +0000

    register route improved. and template html files filled up with more content

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 import json
-from helpers import get_review_count
+from helpers import get_review_counts, error_message
 from flask import Flask, session, request,render_template,redirect
 from flask_session import Session
 from sqlalchemy import create_engine
@@ -25,4 +25,31 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "blu"
+    return get_review_counts("0743454553")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return error_message("please provide username")
+        if not request.form.get("password"):
+            return error_message("please provde password")
+        if request.form.get("password") != request.form.get("password_confirmation"):
+            return error_message("password doesn't match")
+        
+        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                    { "username": request.form.get("username"), "password": request.form.get("password")})
+        db.commit()
+        return render_template("index.html")
+    else:
+        return render_template("register.html")
+    return render_template("register.html") 
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("login.html")
+
