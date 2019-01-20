7d107d9209d54cd9745df40f98f08f9a728eafc5
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2019-01-20 18:48:12 +0000

    imported goodreads API

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os
+import requests
 
-from flask import Flask, session
+from flask import Flask, session, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -16,11 +17,17 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
+# Set up databaseimport requests
+
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
+res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"GACy0ylLEfy4uReswJrGOw": "KEY", "isbns": "9781632168146"})
+if res.status_code != 200:
+    raise Exception("ERROR: API reqsuet unsuccesful")
+print(res.json())
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return "API"
+
